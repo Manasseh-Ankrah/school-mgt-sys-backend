79a46eb8d18d4f23c7783b43b688e8935ef71b12
Manasseh-Ankrah <solomonankrah24@gmail.com>
2022-02-03 03:18:49 +0000

    testing jwt key in prod

--- middleware/auth.js
+++ middleware/auth.js
@@ -8,7 +8,10 @@ const auth = (req, res, next) => {
       return res
         .status(401)
         .json({ msg: "No authentication token, access denied" });
-    const verified = jwt.verify(token, process.env.JWT_SECRET);
+    const verified = jwt.verify(
+      token,
+      "ef3ee8a527ee80718e822c040d24998b833aba902e26e3adce3b571786f9a39753f60cfa1917d26df04b03df8ca29cb851f3b81559782445d15e6a10ec630005"
+    );
     if (!verified)
       return res
         .status(401)
