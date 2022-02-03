cd1fc50d8940ffc5b47902c41e0b10a024eda5b8
Manasseh-Ankrah <solomonankrah24@gmail.com>
2022-02-03 02:25:20 +0000

    third commit

--- server.js
+++ server.js
@@ -6,15 +6,14 @@ const Admin = require("./routes/admin");
 const Student = require("./routes/student");
 const app = express();
 require("dotenv").config();
+// Port variable
+const PORT = process.env.PORT || 5000;
 
 // Middlewares
 app.use(cors());
 app.use(express.json());
 app.use("/admin", Admin);
 app.use("/student", Student);
-
-// Port variable
-const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
   res.send("School Management Backend is ACTIVE!!");
